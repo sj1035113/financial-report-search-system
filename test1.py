4556b5de4a863867c4867fe227d422c1b8405878
sj1035113 <sj1035113zxc@gmail.com>
2023-08-12 19:14:45 +0800

    json練習

--- test1.py
+++ test1.py
@@ -1,8 +1,48 @@
 import json
+class person_info():
+    def __init__(self, birthday, name,data_list,data_dict={}):
+        self.name = name
+        self.data_dict = data_dict
+        self.birthday = birthday
+        self.data_list = data_list
 
-## 讀取JSON檔
-with open('test.json', 'r') as f:
-  ## 轉成Python Dict
-  python_dict = json.load(fp = f)
-print("Python Dict: ", python_dict)
-print("Type: ", type(python_dict))
+    def get_birthday(self):
+        return self.birthday
+    
+    def get_name(self):
+        return self.name
+    
+    def creat_newblock(self):
+        self.data_dict = {
+            'name' : self.name,
+            'birthday' : self.birthday
+        }
+        return self.data_dict
+    
+    def transport_to_json(self):
+        self.data_list.append(self.data_dict)
+        with open("test.json",'w') as file:
+            json.dump(self.data_list,file,indent=4)
+
+    def print_list(self):
+        if self.data_list:
+            print("data_list")
+        else:
+            print("尚未獲取資料")
+            
+    
+
+def main():
+    name = input("輸入姓名")
+    birthday = input("輸入生日")
+    with open('test.json','r') as file:
+        data_list = json.load(file)
+
+    person = person_info(name, birthday,data_list)
+    person.creat_newblock()
+    person.transport_to_json()
+    print(person.print_list())
+    print("成功結束")
+
+if __name__ =="__main__":
+    main()
--- test2.py
+++ test2.py
@@ -1,7 +1,47 @@
 import json
-with open('test.json','r') as file:
-    data = json.load(file)
-    print(data)
+
+class person_info():
+    def __init__(self, birthday, name, data_list):
+        self.name = name
+        self.birthday = birthday
+        self.data_list = data_list
+
+    def get_birthday(self):
+        return self.birthday
+    
+    def get_name(self):
+        return self.name
+    
+    def creat_newblock(self):
+        data_dict = {
+            'name': self.name,
+            'birthday': self.birthday
+        }
+        self.data_list.append(data_dict)
+    
+    def transport_to_json(self):
+        with open("test.json", 'w') as file:
+            json.dump(self.data_list, file, indent=4)  # 使用 indent 參數使 JSON 文件格式化，便於閱讀
+            file.write("\n")
+
+def main():
+    name = input("輸入姓名")
+    birthday = input("輸入生日")
+    with open('test.json', 'r') as file:
+        data_list = json.load(file)
+    
+    # 創建 person_info 實例並操作
+    person = person_info(birthday, name, data_list)
+    person.creat_newblock()
+    person.transport_to_json()
+    
+    print(data_list)
+    print("成功結束")
+
+if __name__ == "__main__":
+    main()
+
+
 
 
 
